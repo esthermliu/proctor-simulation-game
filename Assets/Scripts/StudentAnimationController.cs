084d785fc9d668291392877cdfe0df30c845907e
Esther Liu <estliu@cs.washington.edu>
2026-01-21 14:13:04 -0800

    Initial commit

---
+++ Assets/Scripts/StudentAnimationController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class StudentAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject nextStudent;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Admit()
+    {
+        animator.SetTrigger("Admit");
+    }
+
+    public void Deny()
+    {
+        animator.SetTrigger("Deny");
+    }
+
+    // CALLED BY ANIMATION EVENT
+    public void OnExitAnimationFinished()
+    {
+        gameObject.SetActive(false);
+        SpawnNextStudent();
+    }
+
+
+    public void SpawnNextStudent()
+    {
+        Debug.Log("SPAWNING NEXT STUDENT");
+        if (nextStudent != null)
+        {
+            nextStudent.SetActive(true);
+
+            // Trigger entrance animation
+            Animator anim = nextStudent.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.SetTrigger("Entrance");
+            }
+        }
+    }
+}
